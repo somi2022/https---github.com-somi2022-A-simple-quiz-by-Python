de80e2bb1e9e6b3757e85d5fc0b4ea34d80bee9c
Soulmaz <somisomi76@yahoo.com>
2022-07-07 21:24:10 +0200

    Now after repeating questions and change them put a score for each right anser

--- Python Project.py
+++ Python Project.py
@@ -1,4 +1,4 @@
-
+from cgi import print_arguments
 
 
 print("Welcome to my quiz!")
@@ -6,22 +6,28 @@ playing = input("Do you want to play?" )
 if playing != "yes":
     quit()
 print("Okay! Lets play!")
+score = 0
 answer = input("Where is the capital of Italy? ")
 if answer == "Rome":
         print("Correct!")
+        score += 1
 else:
         print("Incorrect!")
 answer = input("Where is the capital of Norway? ")
 if answer == "Oslo":
         print("Correct!")
+        score += 1
 else:
         print("Incorrect!")
 answer = input("Where is the capital of Japan? ")
 if answer == "Tokyo":
         print("Correct!")
+        score += 1
 else:
         print("Incorrect!")
-
-
-
-
+answer = input("Where is the capital of France? ")
+if answer == "Paris":
+        print("Correct!")
+        score += 1
+else:
+        print("Incorrect!")
