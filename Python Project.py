236c8a2a99130584c277255afb34f8217d47dbea
Soulmaz <somisomi76@yahoo.com>
2022-07-07 20:48:28 +0200

    First question

--- Python Project.py
+++ Python Project.py
@@ -6,3 +6,7 @@ playing = input("Do you want to play?" )
 if playing != "yes":
     quit()
 print("Okay! Lets play!")
+answer = input("Where is the capital of Italy? ")
+if answer == "Rome":
+        print("Correct!")
+
